bfcd57a4b8311544a3990dd008f2bc8c3ab123bb
joeyvlin <joeyvlin@gmail.com>
2014-09-03 10:50:51 -0700

    first commit

---
+++ src/com/codepath/simpletodo/TodoActivity.java
@@ -0,0 +1,79 @@
+package com.codepath.simpletodo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class TodoActivity extends Activity {
+	
+	private ArrayList<String> todoItems;
+	private ArrayAdapter<String> todoAdapter;
+	private ListView lvItems;
+	private EditText etNewItem;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo);
+		etNewItem = (EditText)findViewById(R.id.etNewItem);
+		lvItems = (ListView)findViewById(R.id.lvTodoItem);
+		readItems();
+		todoAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, todoItems);
+		lvItems.setAdapter(todoAdapter);
+		setupListViewListener();
+	}
+	
+	private void setupListViewListener() {
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View item, int position, long id) {
+				todoItems.remove(position);
+				todoAdapter.notifyDataSetChanged();
+				writeItems();
+				return false;
+			}
+			
+		});
+	}
+	
+	private void readItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException e) {
+			todoItems = new ArrayList<String>();
+		}
+	}
+	
+	private void writeItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			FileUtils.writeLines(todoFile, todoItems);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onAddedItem(View v) {
+		String itemText = etNewItem.getText().toString();
+		todoAdapter.add(itemText);
+		etNewItem.setText("");
+		writeItems();
+	}
+}
